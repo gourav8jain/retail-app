83c12e97914f5102d6d0bac6bea5b4b13ac69af4
Gourav Jain <gjain@xebia.com>
2017-06-28 12:02:38 +0530

    #retail-app : Refactoring of the code -- delete un-used namespaces.

--- RetailApp.WebApi/RetailApp.Common/Infrastructure/Common/Logging/ILogger.cs
+++ RetailApp.WebApi/RetailApp.Common/Infrastructure/Common/Logging/ILogger.cs
@@ -11,11 +11,6 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using log4net;
 
 namespace RetailApp.Common.Infrastructure.Common.Logging
